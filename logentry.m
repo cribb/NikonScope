d2d2956c5b5281a053f44c0a2365fd5b2dbed5ad
Jeremy Cribb <cribb@cs.unc.edu>
2020-09-03 15:31:56 -0400

    Added function that serves as a formatted logentry for other functions in the project.

---
+++ logentry.m
@@ -0,0 +1,16 @@
+function logentry(txt)
+% function for writing out stderr log messages
+
+    [ST,I] = dbstack;
+    logtime = clock;
+    logtimetext = [ '(' num2str(logtime(1),  '%04i') '.' ...
+                   num2str(logtime(2),        '%02i') '.' ...
+                   num2str(logtime(3),        '%02i') ', ' ...
+                   num2str(logtime(4),        '%02i') ':' ...
+                   num2str(logtime(5),        '%02i') ':' ...
+                   num2str(floor(logtime(6)), '%02i') ') '];
+     headertext = [logtimetext ST(end).name ': '];
+     
+     fprintf('%s%s\n', headertext, txt);
+     
+     return;    
